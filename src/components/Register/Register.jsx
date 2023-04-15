2b0ac64d7d088fc556f42a47dbee0676342c05a3
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-15 16:35:59 +0200

    59-4 Create new user using email and password on firebase

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,16 +1,65 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../../firebase/firbase.config';
+
+const auth = getAuth(app);
+
 const Register = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
-    console.log(`email : ${email} and password : ${password}`);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const loggedUser = userCredential.user;
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+      });
   };
   return (
     <div className="">
-      <Form onSubmit={handleSubmit} className="w-50 mx-auto">
+      <h2 className="fs-3 text-center">Please login</h2>
+      <form onSubmit={handleSubmit} className="w-50 mx-auto">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
+
+// react component comment out
+
+/* <Form onSubmit={handleSubmit} className="w-50 mx-auto">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -33,9 +82,4 @@ const Register = () => {
         <Button variant="primary" type="submit">
           Register
         </Button>
-      </Form>
-    </div>
-  );
-};
-
-export default Register;
+      </Form> */
